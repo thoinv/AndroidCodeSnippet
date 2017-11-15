9100166d34211691e88df55a7e60bd30006c78a1
Thời Nguyễn <thoinv@segu.vn>
2017-11-15 15:01:34 +0700

    [update] animations/VCAnimationUtils.java

--- animations/VCAnimationUtils.java
+++ animations/VCAnimationUtils.java
@@ -32,4 +32,24 @@ public class VCAnimationUtils {
         });
         viewGroup.startAnimation(animation);
     }
+
+    public void runAnimation() {
+        Animation alphaAnimation = new AlphaAnimation(0.0f, 1.0f);
+        alphaAnimation.setInterpolator(new DecelerateInterpolator(1.0f));
+        alphaAnimation.setDuration(100);
+        alphaAnimation.setFillAfter(true);
+
+        AnimationSet animationSet = new AnimationSet(true);
+
+        alphaAnimation = new TranslateAnimation(1, 0.0f, 1, 0.0f, 1, -0.6f, 1, 0.0f);
+        alphaAnimation.setInterpolator(new DecelerateInterpolator(1.0f));
+        alphaAnimation.setDuration(1000);
+        alphaAnimation.setFillAfter(true);
+
+        animationSet.addAnimation(alphaAnimation);
+        animationSet.addAnimation(alphaAnimation);
+
+        setVisibility(View.VISIBLE);
+        startAnimation(animationSet);
+    }
 }
